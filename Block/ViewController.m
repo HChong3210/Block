5c11146cbbb2ae5eb6afec149b7982dc7e56d48a
HChong <hchong7557@gmail.com>
2016-08-10 16:37:18 +0800

    Initial Commit

---
+++ Block/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Block
+//
+//  Created by HChong on 16/8/10.
+//  Copyright © 2016年 HChong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Block
+//
+//  Created by HChong on 16/8/10.
+//  Copyright © 2016年 HChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
